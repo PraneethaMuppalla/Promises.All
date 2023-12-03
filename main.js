72a24d4354dc1a6604e5b0b9021b3dd6f786a875
PraneethaMuppalla <lakshmimuppalla2453@gmail.com>
2023-12-03 11:19:26 +0530

    Promise.All

---
+++ main.js
@@ -0,0 +1,43 @@
+let usersLastActivityTime = new Date().toLocaleString();
+console.log("time before" + usersLastActivityTime);
+let posts = [{ title: "Post 1" }, { title: "Post 2" }];
+function updateLastUserActivityTime() {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      usersLastActivityTime = new Date().toLocaleString();
+      res();
+    }, 1000);
+  });
+}
+
+function createNewPost() {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      posts.push({ title: "New post" });
+      res();
+    }, 3000);
+  });
+}
+
+function deletePost() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const poppedElement = posts.pop();
+      resolve();
+    }, 1000);
+  });
+}
+
+function printPost() {
+  posts.forEach((post) => {
+    console.log(post.title);
+  });
+}
+
+Promise.all([updateLastUserActivityTime(), createNewPost()]).then(() => {
+  printPost();
+  deletePost().then(() => {
+    printPost();
+  });
+  console.log(usersLastActivityTime);
+});
